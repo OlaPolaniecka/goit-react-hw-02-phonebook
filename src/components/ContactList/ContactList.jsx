f4e18bb43ec942f44efc5f9b5685d529200fe581
Ola Połaniecka <97olka@gmail.com>
2024-01-24 10:05:53 +0100

    work done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,9 @@
+import PropTypes from 'prop-types';
+
 const ContactListItem = ({ id, name, number, onRemove }) => {
   return (
     <li>
-      {name}: {number} <button onCLick={() => onRemove(id)}>Delete</button>
+      {name}: {number} <button onClick={() => onRemove(id)}>Delete</button>
     </li>
   );
 };
@@ -11,10 +13,19 @@ const ContactList = ({ contacts, onRemove }) => {
   return (
     <ul>
       {contacts.map(contact => (
-        <ContactListItem {...contact} onRemove={onRemove} />
+        <li key={contact.id}>
+          <ContactListItem {...contact} onRemove={onRemove} />
+        </li>
       ))}
     </ul>
   );
+};
+
+ContactList.prototype = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  number: PropTypes.number,
+  onRemove: PropTypes.func,
 };
 
 export default ContactList;
